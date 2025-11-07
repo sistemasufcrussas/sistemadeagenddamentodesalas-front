20ae2dec826431ea678c6bc544f3f31c1ee57895
Antonio Lisboa de Carvalho <104227926+LisboaAnt@users.noreply.github.com>
2025-11-07 19:26:46 -0300

    Remover pasta backend do repositório do frontend

--- backend/pages/_app.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-// Arquivo necessário para evitar erros de build do Next.js
-// Este projeto usa apenas API routes, não páginas React
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
-
